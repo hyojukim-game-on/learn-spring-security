38d1d63ff6251193c95c7b516633b35529c506e3
김효주 <hyojukim.dev@gmail.com>
2024-06-28 15:03:51 +0900

    Feat: 생성 및 수정 기능 완성

--- tuser/src/main/java/com/toy/tuser/controller/BlogViewController.java
+++ tuser/src/main/java/com/toy/tuser/controller/BlogViewController.java
@@ -1,11 +1,15 @@
 package com.toy.tuser.controller;
 
+import com.toy.tuser.domain.Article;
 import com.toy.tuser.dto.ArticleListViewResponse;
+import com.toy.tuser.dto.ArticleViewResponse;
 import com.toy.tuser.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,5 +26,25 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable("id") Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(value="id", required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 }
