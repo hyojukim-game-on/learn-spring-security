4541bdaeb01bdb371876f76c60702bd76f950a83
김효주 <hyojukim.dev@gmail.com>
2024-07-01 16:15:55 +0900

    Fix: Login 에러 수정중

--- tuser/src/main/java/com/toy/tuser/config/WebSecurityConfig.java
+++ tuser/src/main/java/com/toy/tuser/config/WebSecurityConfig.java
@@ -6,69 +6,62 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @RequiredArgsConstructor
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
-    private final UserDetailService userDetailService;
+    private final UserDetailService userService;
 
-     // 스프링 시큐리티 기능 비활성화
     @Bean
     public WebSecurityCustomizer configure() {
         return (web) -> web.ignoring()
                 .requestMatchers(toH2Console())
                 .requestMatchers("/static/**");
-
     }
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return userDetailService;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    // 특정 HTTP 요청에 대한 웹 기반 보안 구성
-    // 책에 나와 있는 코드가 deprecated 되어서 공식 Spring 코드 참고해서 수정함
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeRequests((authorizeHttpRequests) -> // 인증, 인가 설정
-                        authorizeHttpRequests
-                                .requestMatchers("/login", "/signup", "/user")
-                                .permitAll()
-                )
-                .formLogin((formLogin) -> // 폼 기반 로그인 설정
-                        formLogin
-                                .loginPage("/login")
+                .authorizeRequests((authorizeRequests)->
+                        authorizeRequests.requestMatchers("/login", "/signup", "/user").permitAll()
+                                .anyRequest().authenticated())
+                .formLogin((formLogin)->
+                        formLogin.loginPage("/login")
                                 .defaultSuccessUrl("/articles"))
-                .logout((logout) -> // 로그아웃 설정
-                        logout
-                                .logoutSuccessUrl("/login")
-                                .invalidateHttpSession(true)
-                )
-                .csrf((csrf) -> csrf.disable())
+                .logout((logout)->
+                        logout.logoutSuccessUrl("/login")
+                                .invalidateHttpSession(true))
+                .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth,
+                                                       UserDetailsService userDetailsService,
+                                                       BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+        return auth.build();
+    }
+
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
 }
