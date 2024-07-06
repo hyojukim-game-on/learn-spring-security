fce4f4f76473d4ac06418452863a057a31f09509
김효주 <hyojukim.dev@gmail.com>
2024-07-06 22:48:20 +0900

    Refactor: OAuth2 로그인

--- tuser/src/main/java/com/toy/tuser/config/WebOAuthSecurityConfig.java
+++ tuser/src/main/java/com/toy/tuser/config/WebOAuthSecurityConfig.java
@@ -10,8 +10,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,28 +26,31 @@ import static org.springframework.boot.autoconfigure.security.servlet.PathReques
 
 @RequiredArgsConstructor
 @Configuration
+@EnableWebSecurity
 public class WebOAuthSecurityConfig {
     private final OAuth2UserCustomService oAuth2UserCustomService;
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
     private final UserService userService;
-    
+
     @Bean
     public WebSecurityCustomizer configure() { // 스프링 시큐리티 기능 비활성화
         return (web) -> web.ignoring()
                 .requestMatchers(toH2Console())
                 .requestMatchers("/img/**", "/css/**", "/js/**");
     }
-    
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // 토큰 방식으로 인증을 하기 때문에 기존에 사용하던 폼로그인, 세션 비활성화
-        http.csrf().disable()
-                .httpBasic().disable()
-                .formLogin().disable()
-                .logout().disable();
-        http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http
+                .csrf((csrf)-> csrf.disable())
+                .httpBasic((httpBasic)-> httpBasic.disable())
+                .formLogin((formLogin)->formLogin.disable())
+                .logout((logout)-> logout.disable());
+        http
+                .sessionManagement((sessionManagement)->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         // 헤더를 확인할 커스텀 필터 추가
         http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
@@ -55,25 +61,36 @@ public class WebOAuthSecurityConfig {
                 .requestMatchers("/api/**").authenticated()
                 .anyRequest().permitAll();
 
-        http.oauth2Login()
-                .loginPage("/login")
-                .authorizationEndpoint()
-                // Authorizaation 요청과 관련된 상태 저장
-                .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository())
-                .and()
-                .successHandler(oAuth2SuccessHandler())
-                .userInfoEndpoint()
-                .userService(oAuth2UserCustomService);
+        // oauth2 로그인 관련 설정
+        http.oauth2Login(
+                (httpSecurityOAuth2LoginConfigurer -> {
+                    httpSecurityOAuth2LoginConfigurer.loginPage("/login");
+                    httpSecurityOAuth2LoginConfigurer.authorizationEndpoint(
+                            (authorizationEndpointConfig -> authorizationEndpointConfig
+                                    .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository()))
+                    );
+                    httpSecurityOAuth2LoginConfigurer.successHandler(oAuth2SuccessHandler());
+                    httpSecurityOAuth2LoginConfigurer.userInfoEndpoint(
+                                    (userInfoEndpointConfig -> userInfoEndpointConfig
+                                            .userService(oAuth2UserCustomService))
+                    );
+                })
+        );
 
-        http.logout()
-                .logoutSuccessUrl("/login");
+        // 로그아웃 관련 설정
+        http.logout(
+                (httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutSuccessUrl("/login"))
+        );
 
         // /api 로 시작하는 url 인 경우 401 상태 코드를 반환하도록 예외 처리
-        http.exceptionHandling()
-                .defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
-                        new AntPathRequestMatcher("/api/**"));
-        return http.build();
+        http.exceptionHandling(
+                (httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer.defaultAuthenticationEntryPointFor(
+                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+                        new AntPathRequestMatcher("/api/**")
+                ))
+        );
 
+        return http.build();
     }
 
     @Bean
